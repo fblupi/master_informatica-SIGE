000f9e3323934ac465d38126019f6b0ea3cb1db8
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2017-04-11 12:05:19 +0200

    Árboles de decisión #9

--- P1/ArbolesDeDecisionSinPreprocesamiento.R
+++ P1/ArbolesDeDecisionSinPreprocesamiento.R
@@ -12,15 +12,8 @@ library(rattle)
 library(rpart.plot)
 library(RColorBrewer)
 
-# Recreate the gender model
-fit <- rpart(Survived ~ Sex, data=train, method="class")
-fancyRpartPlot(fit)
-
 # Build a deeper tree
 fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train, method="class")
-# Plot it with base-R
-plot(fit)
-text(fit)
 # And then make it look better with fancyRpartPlot!
 fancyRpartPlot(fit)
 
@@ -38,9 +31,3 @@ fancyRpartPlot(fit)
 Prediction <- predict(fit, test, type = "class")
 submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
 write.csv(submit, file = "bigdecisiontree.csv", row.names = FALSE)
-
-# Manually trim a decision tree
-fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, data=train,
-             method="class", control=rpart.control(minsplit=2, cp=0.005))
-new.fit <- prp(fit,snip=TRUE)$obj
-fancyRpartPlot(new.fit)
